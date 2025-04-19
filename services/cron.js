687951c4f46a57461b1b0aa557340d824c430840
Maxim <maxime.heim3@gmail.com>
2025-04-19 10:36:07 +0200

    resetup cron

--- services/cron.js
+++ services/cron.js
@@ -3,22 +3,15 @@ const {main} = require('../server');
 const config = require('../config/config');
 
 // Setup the cron job
-// cron.schedule(config.cron.schedule, async () => {
-//     console.log(`Running scheduled CVE import job at ${new Date().toISOString()}`);
-//     try {
-//         await main();
-//         console.log(`Completed scheduled CVE import job at ${new Date().toISOString()}`);
-//     } catch (error) {
-//         console.error('Error in scheduled CVE import job:', error.message);
-//     }
-// });
-
-function main2() {
-    main();
-    console.log(`Completed scheduled CVE import job at ${new Date().toISOString()}`);
-}
-
-main2();
+cron.schedule(config.cron.schedule, async () => {
+    console.log(`Running scheduled CVE import job at ${new Date().toISOString()}`);
+    try {
+        await main();
+        console.log(`Completed scheduled CVE import job at ${new Date().toISOString()}`);
+    } catch (error) {
+        console.error('Error in scheduled CVE import job:', error.message);
+    }
+});
 
 console.log(`CVE importer cron job started with schedule: ${config.cron.schedule}`);
 console.log('Press Ctrl+C to stop');
